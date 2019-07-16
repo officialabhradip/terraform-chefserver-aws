956d321f919c98c20b93204dcb7a2d41876bdc04
officialabhradip <52069187+officialabhradip@users.noreply.github.com>
2019-07-16 14:55:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ secret_key = ""
 region = "ap-southeast-1"
 }
 
-resource "aws_instance" "cicdvm" {
+resource "aws_instance" "chefserver" {
 ami = "ami-04613ff1fdcd2eab1"
 instance_type = "t2.medium"
 root_block_device{
