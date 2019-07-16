a7a625b36b76bd303999152eff3fd315518da322
officialabhradip <52069187+officialabhradip@users.noreply.github.com>
2019-07-16 14:16:57 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws {
+access_key = ""
+secret_key = ""
+region = "ap-southeast-1"
+}
+
+resource "aws_instance" "cicdvm" {
+ami = "ami-04613ff1fdcd2eab1"
+instance_type = "t2.medium"
+root_block_device{
+volume_size = "50"
+}
+provisioner "remote-exec" {
+    inline = [
+        ""
+    ]
+  }
+  
+}
